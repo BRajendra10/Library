674c90a7673a861a98b7199d318ec2fb0032ffe2
13-Bhupendra <bhupendrakp2008@gmail.com>
2025-08-18 11:38:38 +0530

    Fetched members data

--- src/screens/Members.jsx
+++ src/screens/Members.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Members() {
+const [data , setdata] = useState([])
+
+  const HandleData = async ()=> {
+    try {
+        const res = await axios.get("https://book-server-ahrs.onrender.com/members")
+        setdata(res.data)
+    } catch (error) {
+      console.log(error) 
+    }
+  }
+
+
+  useEffect(()=> {
+    HandleData()
+  } ,[])
+
   return (
     <div className="w-full min-h-screen flex justify-center items-center bg-blue-200">
         <h1 className="text-5xl">Members</h1>
